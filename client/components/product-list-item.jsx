53c86967a63f1328ecb86aaffea59f919f3798ab
jinho6225 <jinho6225@hotmail.com>
2020-03-09 23:15:05 +0000

    created header, product-list-item, product-list component, create method getProducts and render to the page

---
+++ client/components/product-list-item.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const ProductListItem = props => {
+  const { product } = props;
+  function dollar(num) {
+    const arr = num.toString().split('');
+    arr.splice(arr.length - 2, 0, '.');
+    return Number(arr.join(''));
+  }
+
+  return (
+    <div className="col-4 card-deck my-2">
+      <div className="card">
+        <img src={product.image} className="card-img" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">{product.name}</h5>
+          <p className="card-text text-muted">{`$ ${dollar(product.price).toFixed(2)}`}</p>
+          <p className="card-text">{product.shortDescription}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default ProductListItem;
