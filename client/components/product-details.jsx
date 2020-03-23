c1e4692e18b9ebddc24addec27a4395cbf7ea503
jinho6225 <jinho6225@hotmail.com>
2020-03-23 08:13:23 +0000

    favicon update, product detail css updated

--- client/components/product-details.jsx
+++ client/components/product-details.jsx
@@ -26,34 +26,56 @@ class ProductDetails extends Component {
     const { product } = this.state;
     if (product !== null) {
       return (
-        <div className="container d-flex flex-column">
-          <div className="row d-flex flex-row justify-content-center" id={product.productId}>
-            <div className="w-100 px-5 d-flex flex-column justify-content-lg-start justify-content-md-center justify-content-sm-center justify-content-center col-4 col-12 order-1 col-lg-5 order-lg-1">
-              <p className="text-muted pointer" onClick={() => {
-                this.props.backTo();
-              }}> <i className="fas fa-angle-left "></i> Back to catalog </p>
-              <p><img src={product.image} className="detail-img" alt="..." /></p>
+        <div className="container py-5">
+          <div
+            className="container py-3 bg-white border rounded shadow"
+            id={product.productId}
+          >
+            <div className="mb-3">
+              <a
+                className="text-muted pointer mb-3"
+                onClick={() => {
+                  this.props.backTo();
+                }}
+              >
+                {' '}
+                <i className="fas fa-chevron-circle-left "></i> Back to catalog
+              </a>
             </div>
-            <div className=" w-100  px-5 d-flex flex-column justify-content-lg-center justify-content-md-center justify-content-sm-center justify-content-center  col-8 col-12 order-2 col-lg-7 order-lg-2">
-              <h5 className="card-title">{product.name}</h5>
-              <p className="card-text text-muted">{`$${(product.price / 100).toFixed(2)}`}</p>
-              <p className="card-text">{product.shortDescription}</p>
-              <p className="card-text" onClick={() => {
-                this.props.addToCard(product);
-              }} ><button type="button" className="btn btn-primary">Add to Cart</button></p>
-
+            <div className="row mb-3">
+              <div className="col-md-5">
+                <img
+                  src={product.image}
+                  className="w-100 detail-img slide-in"
+                  alt={product.name}
+                />
+              </div>
+              <div className="col-md-7 p-2">
+                <h3 className="card-title slide-in">{product.name}</h3>
+                <h4 className="card-text slide-in">{`$${(
+                  product.price / 100
+                ).toFixed(2)}`}</h4>
+                <p className="card-text slide-in">{product.shortDescription}</p>
+                <p
+                  className="card-text slide-in"
+                  onClick={() => {
+                    this.props.addToCard(product);
+                  }}
+                >
+                  <button type="button" className="btn text-white addBtn">
+                    Add to Cart
+                  </button>
+                </p>
+              </div>
             </div>
-          </div>
-          <div className="row ">
-            <p className="my-3 px-5">{product.longDescription}</p>
+            <div>
+              <p className="slide-in">{product.longDescription}</p>
+            </div>
           </div>
         </div>
       );
-
     } else {
-      return (
-        <div>watsup</div>
-      );
+      return <div>watsup</div>;
     }
   }
 }
