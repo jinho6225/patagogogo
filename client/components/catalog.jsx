18775654ab4c9c8587d9076ac09ac053c7269c9e
jinho6225 <jinho6225@hotmail.com>
2020-03-29 06:13:34 +0000

    patagogog catalog carousel, added react hook useState, useEffect

--- client/components/catalog.jsx
+++ client/components/catalog.jsx
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function Catalog() {
+  const [index, setIndex] = useState(1);
+  useEffect(() => {
+    const id = setInterval(() => {
+      if (index <= 5) {
+        setIndex(index + 1);
+      } else {
+        setIndex(index - 6 + 1);
+      }
+    }, 3000);
+    return () => {
+      clearInterval(id);
+    };
+  }, [index]);
+
   return (
     <div className="catalog-item">
-      <div className="h-100 w-100 carousel-slide slide-2">
-        <img
-          src="./images/catalog3.jpeg"
-          className="d-block w-100 carousel-image"
-        />
+      <div className="h-100 w-100 catalog-slide fade-in">
+        <img src={`./images/catalog${index}.jpeg`} className="d-block w-100" />
       </div>
     </div>
   );
