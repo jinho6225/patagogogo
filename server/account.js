6638cfcf8dbd22217cdc0b973e7ba4b634959423
jinho6225 <jinho6225@hotmail.com>
2020-06-04 21:45:42 -0700

    installed @hapi/joi

---
+++ server/account.js
@@ -0,0 +1,32 @@
+const db = require('./database');
+const crypto = require('crypto');
+
+function hash(password) {
+  return crypto
+    .createHmac('sha256', process.env.SECRET_KEY)
+    .update(password)
+    .digest('hex');
+}
+
+const Account = {
+  registerLocal: async (req, res) => {
+    const { email, pwd } = req;
+    const qry =
+      'insert into user (email, pwd, submission_date) values (?, ?, NOW())';
+    let register = null;
+    try {
+      register = await db.query(qry, [email, hash(pwd)]);
+    } catch (e) {
+      console.error(e);
+    }
+    return register.values[0];
+  },
+  localLogin: async (req, res) => {},
+
+  validatePassword: function (pwd) {
+    const hashed = hash(pwd);
+    return this.pwd === hashed;
+  },
+};
+
+module.exports = Account;
---
+++ server/auth.router.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const auth = express.Router();
+const authCtrl = require('./auth.controller.js');
+
+auth.post('/register/local', authCtrl.localRegister);
+
+auth.get('/exists/email/:value', authCtrl.exists);
+
+auth.post('/login/local', authCtrl.localLogin);
+auth.post('/logout', authCtrl.logout);
+
+module.exports = auth;
