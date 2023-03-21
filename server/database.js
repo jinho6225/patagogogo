6e6ce0682d6e42442cc27ba469d5430112526edd
Jinho Myung <jinhomyung@Jinhos-MBP.lan>
2023-03-21 10:17:28 -0700

    configuration update

--- server/database.js
+++ server/database.js
@@ -1,40 +1,21 @@
-const { Pool, Client } = require('pg');
+const pg = require('pg');
 
-const credentials = {
-  user: 'root',
+const db = new pg.Pool({
   host: 'postgresql',
-  database: 'patagogodb',
+  user: 'root',
   password: 'root',
+  database: 'patagogodb',
   port: 5432,
-};
+  min: 5,
+  max: 50,
+})
 
-const db = new Client(credentials);
-console.log(db, 'db in database.js')
-// Connect with a connection pool.
-async function poolDemo() {
-  const pool = new Pool(credentials);
-  const now = await pool.query('SELECT NOW()');
-  await pool.end();
-  return now;
-}
-
-// Connect with a client.
-async function clientDemo() {
-  const client = new Client(credentials);
-  await client.connect();
-  const now = await client.query('SELECT NOW()');
-  await client.end();
-  return now;
-}
-
-// Use a self-calling function so we can use async / await.
-
-(async () => {
-  const poolResult = await poolDemo();
-  console.log('Time with pool: ' + poolResult.rows[0].now);
-
-  const clientResult = await clientDemo();
-  console.log('Time with client: ' + clientResult.rows[0].now);
-})();
+db.connect(err => {
+  if (err) {
+    console.log('Failed to connect db ' + err)
+  } else {
+    console.log('Connect to db done!')
+  }
+})
 
 module.exports = db;
